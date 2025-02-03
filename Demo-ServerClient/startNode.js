cdbc889818cc67afcac2b1c4752b190e7f6d24e5
ThinnaphatCSI <thinnaphat.bo@spu.ac.th>
2025-02-03 16:06:34 +0700

    fianl startNode.js commit

--- Demo-ServerClient/startNode.js
+++ Demo-ServerClient/startNode.js
@@ -1,13 +1,19 @@
+
+const os = require('os');
+const host = os.hostname();
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+    res.statusCode = 500;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`CSI203: DIGITAL ARCHITECTURE AND OPERATING SYSTEM\nThis is a sample learning tool for the CSI 203 course.\n\n\tWorkshop #3: startNode.js`);
+    res.end(`CSI203: DIGITAL ARCHITECTURE AND OPERATING SYSTEM\n
+This is a sample learning tool for the CSI 203 course.\n\n
+\tWorkshop #3: startNode.js`);
 });
 
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Hostname: ${host}`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
